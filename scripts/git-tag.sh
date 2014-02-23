1c1d3f4b42635473391e174c49c55fb6250f3b1b
Cris Stanza <crisstanza@note>
2014-02-23 12:48:27 -0300

    Script Git para gerência de tags.

---
+++ scripts/git-tag.sh
@@ -0,0 +1,4 @@
+git tag -a v0.2 -m "Version 0.2"
+git checkout v0.2
+git push origin --tags
+
--- scripts/zip-tag.sh
+++ scripts/zip-tag.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 mkdir ../gh-pages/download
-zip -r ../gh-pages/download/memorize-0.1.zip css fonts img js php sql/ranking.* *.html
+zip -r ../gh-pages/download/memorize-0.2.zip css fonts img js php sql/ranking.* *.html
 
